60cffb0f63d7d36ff8f1485dde90191592e00424
njanchar <145516496+njanchar@users.noreply.github.com>
2024-11-25 20:08:39 -0500

    Add files via upload

---
+++ serial_test.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string.h>
+
+#include <fcntl.h>
+#include <errno.h>
+#include <termios.h>
+#include <unistd.h>
+#include <chrono>
+
+#include <thread>
+
+using namespace std;
+
+int main(){
+    int serial_port = open("/dev/", O_RDWR);
+
+    if(serial_port < 0){
+        printf("Error %i \n", errno, strerror(errno));
+    }
+
+    struct termios tty;
+
+    if(tcgetattr(serial_port, &tty)!=0){
+        printf("Error %i \n", errno, strerror(errno));
+    }
+
+  tty.c_cflag &= ~CSTOPB;
+  tty.c_cflag &= ~CSIZE;
+  tty.c_cflag |= ~CS8;
+  tty.c_cflag &= ~CRTSCTS;
+  tty.c_cflag |= CREAD | CLOCAL;
+
+  tty.c_cflag &= ~ICANON;
+  tty.c_cflag &= ~ECHO;
+  tty.c_cflag &= ~ECHOE;
+  tty.c_cflag &= ~ECHONL;
+  tty.c_cflag &= ~ISIG;
+  tty.c_cflag &= ~(IXON | IXOFF | IXANY);
+  tty.c_cflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL);
+
+  tty.c_oflag &= ~OPOST;
+  tty.c_oflag &= ~ONLCR;
+
+  tty.c_cc[VTIME] = 1;
+  tty.c_cc[VMIN] = 1;
+
+  cfsetispeed(&tty, B115200);
+
+  if(tcsetattr(serial_port, TCSANOW, &tty)!=0){
+    printf("Error %i \n", errno, strerror(errno));
+  }
+
+  while(true){
+    unsigned char msg[] = {'D', 'R'};
+    write(serial_port, msg, sizeof(msg));
+    this_thread::sleep_for(chrono::seconds(1));
+  }
+
+
+}
