b04877347f5eee3e0e1f7dd445aacf1013b6c908
njanchar <145516496+njanchar@users.noreply.github.com>
2024-12-09 16:24:57 -0500

    Add files via upload

--- Serial_Test_Noah.ino
+++ Serial_Test_Noah.ino
@@ -1,9 +1,19 @@
 
-uint8_t read_buf[2];
-uint8_t test_buf[4];
+uint8_t read_buf[4];
+
 uint8_t test_buf2[4];
-int test_num = 305;
-int test_num2 = 455;
+uint8_t test_buf3[20];
+uint8_t write_me[4];
+float test_num = 30.5;
+float test_num2 = 30.0;
+float test_num3 = 50.00;
+float test_num4 = 6500.45;
+float test_num5 = 12345.67;
+int new_num = 300;
+uint8_t test_buf[4];
+uint8_t test_buf4[4];
+uint8_t test_buf5[4];
+uint8_t test_buf6[4];
 
 void setup() {
   // put your setup code here, to run once:
@@ -11,16 +21,38 @@ void setup() {
   Serial.begin(9600);
   memcpy(&(test_buf[0]), &(test_num), sizeof(test_buf));
   memcpy(&(test_buf2[0]), &(test_num2), sizeof(test_buf2));
+  memcpy(&(test_buf4[0]), &(test_num3), sizeof(test_buf4));
+  memcpy(&(test_buf5[0]), &(test_num4), sizeof(test_buf5));
+  memcpy(&(test_buf6[0]), &(test_num5), sizeof(test_buf6));
+  for(int i=0;i<sizeof(test_buf);i++){
+    test_buf3[i] = test_buf[i];
+  }
+  for(int i=0;i<sizeof(test_buf2);i++){
+    test_buf3[i+4] = test_buf2[i];
+  }
+  for(int i=0;i<sizeof(test_buf4);i++){
+    test_buf3[i+8] = test_buf4[i];
+  }
+  for(int i=0;i<sizeof(test_buf5);i++){
+    test_buf3[i+12] = test_buf5[i];
+  }
+   for(int i=0;i<sizeof(test_buf6);i++){
+    test_buf3[i+16] = test_buf6[i];
+  }
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if(Serial.available() > 0){
-    uint8_t incomingByte = Serial.read();
-    Serial.readBytes(read_buf, 2);
+   // uint8_t incomingByte = Serial.read();
+    Serial.readBytes(read_buf, 4);
+    for(int i=0;i<sizeof(read_buf);i++){
+    test_buf3[i+16] = read_buf[i];
+    }
     delay(50);
-    Serial.write(test_buf,sizeof(test_buf));
-    Serial.write(test_buf2,sizeof(test_buf2));
+    Serial.write(test_buf3,sizeof(test_buf3));
+   // Serial.write(test_buf3, sizeof(test_buf3));
   }
  // delay(300);
   //Serial.print('A');
--- serial_test_too.cpp
+++ serial_test_too.cpp
@@ -49,7 +49,7 @@ int main(){
   tty.c_oflag &= ~ONLCR;
 
   tty.c_cc[VTIME] = 10;
-  tty.c_cc[VMIN] = 8;
+  tty.c_cc[VMIN] = 20;
 
   cfsetispeed(&tty, B9600);
   cfsetospeed(&tty, B9600);
@@ -63,9 +63,19 @@ int main(){
    usleep(1000*1000);
    int read_nums[2];
    uint8_t temp_buf[4];
-   int temp_num;
+   float temp_num;
+   uint8_t temp_buf2[4];
+   float temp_num2;
 	uint8_t read_buf[256];
-	uint8_t write_buf[] = {4,7};
+	uint8_t write_buf[4];
+   float send_it = 3.45;
+   memcpy(&(write_buf),&send_it,sizeof(write_buf));
+   float temp_num3;
+   float temp_num4;
+   float temp_num5;
+   uint8_t temp_buf5[4];
+   uint8_t temp_buf3[4];
+   uint8_t temp_buf4[4];
 	memset(&read_buf, '\0', sizeof(read_buf));
 	printf("About to send\n");
 	write(serial_port, write_buf, sizeof(write_buf));
@@ -78,13 +88,26 @@ int main(){
 	return 1;
 	}
 	// memcpy(&(read_nums[0]), &(read_buf[0]), sizeof(read_nums));
-   for(int i=0;i<sizeof(int);i++){
-      temp_buf[i] = read_buf[i];
+   for(int i=0;i<20;i++){
+      if(i<4){temp_buf[i] = read_buf[i];}
+      else if(i<8){temp_buf2[i-4] = read_buf[i];}
+      else if(i<12){temp_buf3[i-8] = read_buf[i];}
+      else if(i<16){temp_buf4[i-12] = read_buf[i];}
+      else if(i<20){temp_buf5[i-16] = read_buf[i];}
    }
+  // temp_buf4[0] = '\0';
+  // temp_buf4[1] = '\0';
+  // temp_buf4[2] = '\0';
+  // temp_buf4[3] = '\0';
    memcpy(&temp_num,&(temp_buf),sizeof(temp_num));
+   memcpy(&temp_num2,&(temp_buf2),sizeof(temp_num2));
+   memcpy(&temp_num3,&(temp_buf3),sizeof(temp_num3));
+   memcpy(&temp_num4,&(temp_buf4),sizeof(temp_num4));
+   memcpy(&temp_num5,&(temp_buf5),sizeof(temp_num5));
 	printf("Read %i bytes.\n", num_bytes);
 	// printf("Recieved message: %i, %i\n", (int)(read_buf[0]), (int)(read_buf[1]));
-   printf("Read two nums: %d, %d \n", temp_num,read_nums[1]);
+   printf("Read two nums: %f, %f , %f , %f, %f\n", temp_num, temp_num2, temp_num3, temp_num4, temp_num5);
+
 	close(serial_port);
 	
 	return 0;
